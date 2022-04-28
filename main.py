114de64250a5c09146b61e5ab62a0a5cace0998c
Sgp1313 <sebastiangarcia@ufm.edu>
2022-04-27 22:47:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 
+from ast import Pass, Return
+
+
 def create_empty_board():
 
     board = []
@@ -14,21 +17,71 @@ def print_board(board):
 
 
 def update_board(board, position, player):
-    # TODO: modificar tablero con nuevo movimiento del jugador en posicion indicada.
-    pass
+
+     board[position[0]][position[1]] = player 
+     return board
+
 
 
 def check_for_winner(board, player):
-    # TODO: evaluar si el jugador indicado ha ganado la partida.
-    pass
+
+    if (board[0][0] == player and board[0][1] == player and board[0][2] == player):
+        return True
+
+    elif (board[1][0] == player and board[1][1] == player and board[1][2] == player):
+        return True
+
+    elif (board[0][0] == player and board[1][1] == player and board[2][2] == player):
+        return True
+
+    elif (board[2][0] == player and board[2][1] == player and board[2][2] == player):
+        return True
+
+    elif (board[0][0] == player and board[1][0] == player and board[2][0] == player):
+        return True
+ 
+    elif (board[0][2] == player and board[1][1] == player and board[2][0] == player):
+        return True
+
+    elif (board[0][1] == player and board[1][1] == player and board[2][1] == player):
+        return True
+
+    elif (board[0][2] == player and board[1][2] == player and board[2][2] == player):
+        return True
+
+
+    else:
+        return False
 
 
 '''
 Testing: 
 '''
 board = create_empty_board()
+
+update_board(board, [1,1], "⭕️")
+
+update_board(board, [2,2], "⭕️")
+
+update_board(board, [1,2], "❌")
+
+update_board(board, [2,1], "❌")
+
+update_board(board, [0,1], "❌")
+
+update_board(board, [0,2], "❌")
+
+update_board(board, [2,1], "❌")
+
+update_board(board, [0,0], "⭕️")
+
+update_board(board, [2,0], "❌")
+
+update_board(board, [1,0], "⭕️")
+
 print_board(board)
 
+pass
 
-
+print("⭕️ WINS!")
 
